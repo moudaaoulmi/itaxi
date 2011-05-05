64d38fa0a5dea78382d7b9681f3cd664680f1f18
joseduartelourenco@gmail.com <joseduartelourenco@gmail.com@30af5f15-cf4c-c251-3ae6-9fca1db68a67>
2011-05-05 15:20:52 +0000

    Initial commit

---
+++ trunk/source/iTaxi/aasma/jade/Customer.java
@@ -0,0 +1,22 @@
+/**
+ * Leiloeiro num leilao do tipo 
+ * first-price sealed-bid auction
+ */
+package aasm.jade.firstPriceSealedBidAuction;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class Customer extends Agent {
+    
+}
